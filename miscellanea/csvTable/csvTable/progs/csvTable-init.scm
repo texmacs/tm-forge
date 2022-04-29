dddd73027193b00f9c9a7cc82d37cb09b5887335
pireddag <pireddag@gmail.com>
2022-04-29 23:34:58 +0200

    csvTable plugin

---
+++ miscellanea/csvTable/csvTable/progs/csvTable-init.scm
@@ -0,0 +1,33 @@
+(texmacs-module (csvTable-init))
+
+(use-modules (csvTable))
+
+;; Mode definitions
+;; See Scheme developer guide, p. 28
+(texmacs-modes
+(in-big-table% (inside? 'big-table) in-text%))
+
+
+;; With this Widget we issue a message to the user when the function is used outside
+;; the planned environments
+(tm-widget (csvTable-message-widget)
+  (vlist
+    (centered
+      (text "Please insert table in plain text")
+      ===
+      (text "or big-table environment"))))
+
+
+;; Map "t a b l e tab" to either a message for the user, if the cursor is outside
+;; all of the environments where it is expected to work, or to the insertion of
+;; a table at the cursor position
+
+(kbd-map
+  ("t a b l e tab"
+   (top-window csvTable-message-widget "Insert table message")))
+
+(kbd-map
+  (:require (and  (or (in-text?) (in-big-table?)) (not (in-table?))))
+  ("t a b l e tab" (choose-file insert-csvTable "choose table file" "")))
+
+
